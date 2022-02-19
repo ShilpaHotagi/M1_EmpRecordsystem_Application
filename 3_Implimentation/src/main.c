b9914f500683f249b6c803f6fa8dcd8469f0e138
ShilpaHotagi <98829965+ShilpaHotagi@users.noreply.github.com>
2022-02-19 01:40:33 -0500

    Update main.c

--- 3_Implimentation/src/main.c
+++ 3_Implimentation/src/main.c
@@ -1,3 +1,13 @@
+/**
+ * @file main.c
+ * @author  Shilpa Hotagi
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-19
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "fun.h" 
 #include<stdio.h>
 int main()
